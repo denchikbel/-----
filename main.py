a863ff0da7f0c65f33b1d4098747e8d4fd73bfd2
Daniil Aleksandrovich <daniilbelyi833@gmail.com>
2026-02-04 01:13:08 +0500

    2

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+
+#функция конфигурирования и запуска бота
+async def main()-> None:
+    #загружаем конфиг в перменную
+    config: Config = load_config()
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format
+    )
+
+bot = Bot(token = config.bot.token)    
