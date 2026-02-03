6f937169672e1b27f391c0cf7e4de678d65aacb7
Daniil Aleksandrovich <daniilbelyi833@gmail.com>
2026-02-03 15:38:38 +0500

    добавил игнор

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from data import *
 from ciasses import *
 from environs import Env
 env = Env()
+env.read_env()
+bot_token = env('BOT_TOKEN')
 
 if __name__=='__main__':
 
     print ('это исполняемый файл')
     print(functions.get_double_number(100))
     print(my_dicy)
+    print(f'токен бота {bot_token}')
 
     myclass()
